b5034cdc9a4e45855d20fb7fd3e6a84e86c3e978
ricardoanzolin <ricardoanzolin@gmail.com>
2016-03-22 20:21:24 -0300

    Hackaton

--- hackathon/js/main.js
+++ hackathon/js/main.js
@@ -38,7 +38,7 @@ $(window).load(function(){
 function init_map() {
     var myOptions = {
         zoom: 14,
-        center: new google.maps.LatLng(-22.902488, -43.175939), //change the coordinates
+        center: new google.maps.LatLng(-26.203740, -52.688162), //change the coordinates
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         scrollwheel: false,
         styles: [{featureType:'all',stylers:[{saturation:-100},{gamma:0.90}]}]
@@ -46,11 +46,8 @@ function init_map() {
     map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
     marker = new google.maps.Marker({
         map: map,
-        position: new google.maps.LatLng(-22.902488, -43.175939) //change the coordinates
+        position: new google.maps.LatLng(-26.203740, -52.688162) //change the coordinates
     });
 
 }
 google.maps.event.addDomListener(window, 'load', init_map);
-
-
-
